fd913031d388abbf5c82f759a668e55d8e7a5685
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-11 14:35:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if block_given?
+i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
+  else
+    print "Hey! No block was given!"
+  end 
+end
