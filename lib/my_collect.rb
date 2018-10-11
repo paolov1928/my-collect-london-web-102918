f7cfaee963ff4061c3023633f5c269241d2e3702
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-11 14:37:14 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,8 @@
 def my_collect(array)
-  if block_given?
 i = 0
   while i < array.length
     yield array[i]
     i = i + 1
   end
   array
-  else
-    print "Hey! No block was given!"
-  end 
 end
