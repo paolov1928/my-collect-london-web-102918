ac9b96f86230c513c0eb0dfdba4dedca990f9f21
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-11 15:00:38 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array)
 i = 0
 array2 = []
   while i < array.length
-    array2.unshift(yield array[i]) .reverse
+    array2.unshift(yield array[i])
     
     i = i + 1
   end
-  array2
+  array2.reverse
 end
